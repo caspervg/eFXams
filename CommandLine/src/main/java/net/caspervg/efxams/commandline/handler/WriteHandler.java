42f319d6c8f7c6984cb762e490da9006c07b6ebc
Casper Van Gheluwe <caspervg@gmail.com>
2014-05-18 19:49:27 +0200

    Write exam to file

--- CommandLine/src/main/java/net/caspervg/efxams/commandline/handler/WriteHandler.java
+++ CommandLine/src/main/java/net/caspervg/efxams/commandline/handler/WriteHandler.java
@@ -27,8 +27,15 @@ package net.caspervg.efxams.commandline.handler;
 import com.google.common.io.Files;
 import net.caspervg.efxams.backend.BackendFactory;
 import net.caspervg.efxams.backend.ExamBackend;
+import net.caspervg.efxams.backend.beans.Exam;
+import net.caspervg.efxams.backend.beans.Question;
+import net.caspervg.efxams.backend.exception.ExamBackendException;
 import net.caspervg.efxams.commandline.argument.Command;
 import net.caspervg.efxams.commandline.argument.CommandWrite;
+
+import java.io.IOException;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class WriteHandler implements CommandHandler {
 
@@ -45,8 +52,75 @@ public class WriteHandler implements CommandHandler {
 
         String fileExtension = Files.getFileExtension(write.getFile().getName());
         ExamBackend backend = BackendFactory.getBackend(fileExtension);
-        System.out.println(backend);
 
-        return true;
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Please enter the name of the examination: ");
+        String examName = in.nextLine();
+
+        System.out.println("Please enter the name of the author of the examination: ");
+        String examAuthor = in.nextLine();
+
+        boolean stopped = false;
+        int counter = 1;
+        List<Question> questionList = new ArrayList<>();
+
+        while(! stopped) {
+            System.out.println(String.format("Enter the title for question %d. Write STOP to stop adding more questions", counter));
+            String questionName = in.nextLine();
+
+            if (! questionName.equals("STOP")) {
+                System.out.println(String.format("Enter the query for question %d", counter));
+                String questionQuery = in.nextLine();
+
+                System.out.println(String.format("Enter the answer for question %d", counter));
+                String questionAnswer = in.nextLine();
+
+                System.out.println(String.format("Enter a comma-delimited list of keywords that the answer to question %d should contain to be counted valid", counter));
+                String allowed = in.nextLine();
+                HashSet<String> allowedList = Arrays.asList(allowed.split(","))
+                        .stream()
+                        .map(String::trim)
+                        .collect(Collectors.toCollection(HashSet::new));
+
+                System.out.println(String.format("Enter a comma-delimited list of keywords that the answer to question %d may not contain to be counted valid", counter));
+                String banned = in.nextLine();
+                HashSet<String> bannedList = Arrays.asList(banned.split(","))
+                        .stream()
+                        .map(String::trim)
+                        .collect(Collectors.toCollection(HashSet::new));
+
+                System.out.println(String.format("Enter a comma-delimited list of hints that the user can receive for question %d", counter));
+                String hints = in.nextLine();
+                LinkedList<String> hintList = Arrays.asList(hints.split(","))
+                        .stream()
+                        .map(String::trim)
+                        .collect(Collectors.toCollection(LinkedList::new));
+
+                questionList.add(new Question.QuestionBuilder(questionName, questionQuery, questionName)
+                        .allowedWords(allowedList)
+                        .bannedWords(bannedList)
+                        .hints(hintList)
+                        .build());
+            } else {
+                stopped = true;
+            }
+        }
+
+        Exam exam = new Exam.ExamBuilder(examName)
+                .author(examAuthor)
+                .questions(questionList)
+                .build();
+
+        try {
+            //noinspection ResultOfMethodCallIgnored
+            write.getFile().createNewFile();
+
+            backend.marshallExam(exam, write.getFile());
+            return true;
+        } catch (ExamBackendException | IOException e) {
+            System.err.println("Could not write exam to file" + e.toString());
+            return false;
+        }
     }
 }
